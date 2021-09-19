51f0679b256ee8c469a71a906bd814ef77761c0a
NikitaKakurin <neyasut87@rambler.ru>
2021-09-19 12:34:42 +0300

    feat carbon dating 26 passing

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,12 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+export default function dateSample(sampleActivity) {
+  if(typeof(sampleActivity)!='string')return false;
+  if(isNaN(sampleActivity))return false;
+  sampleActivity=+sampleActivity;
+  if(sampleActivity<=0 || sampleActivity>15)return false;
+
+  return Math.ceil(Math.log(MODERN_ACTIVITY/sampleActivity)/(0.693/HALF_LIFE_PERIOD));
   // remove line with error and write your code here
 }
